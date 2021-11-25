ca5ad7ca0dcf2e4488f4e4182f65960b2d97f7c3
Lechatelia <lechatelie@stu.xjtu.edu.cn>
2021-11-25 23:20:00 +0800

    add args

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,2 +1,3 @@
 from .model import NeuralNetwork
 from .dataset import MNIST, FashionMNIST
+from .utils import adjust_learning_rate
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plt
 from demo import FashionMNIST
-from demo import NeuralNetwork
+from demo import NeuralNetwork, adjust_learning_rate
+import argparse
+from demo import utils
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def main():
+def main(args):
     # Download training data from open datasets.
     training_data = FashionMNIST(
         root="data",
@@ -26,21 +29,16 @@ def main():
         transform=ToTensor(),
     )
 
-    batch_size = 64
 
     # Create data loaders.
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    train_dataloader = DataLoader(training_data, batch_size=args.batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=args.batch_size)
 
-    for X, y in test_dataloader:
-        print("Shape of X [N, C, H, W]: ", X.shape)
-        print("Shape of y: ", y.shape, y.dtype)
-        break
+    # for X, y in test_dataloader:
+    #     print("Shape of X [N, C, H, W]: ", X.shape)
+    #     print("Shape of y: ", y.shape, y.dtype)
+    #     break
 
-
-
-
-    # Get cpu or gpu device for training.
     
     print(f"Using {device} device")
 
@@ -49,18 +47,28 @@ def main():
     print(model)
 
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    epochs = 5
-    for t in range(epochs):
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.base_lr)
+    elif args.optimizer == 'adam':
+        optimizer = torch.optim.adamw(model.parameters(), lr=args.base_lr)
+
+    for t in range(args.epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
+        train(train_dataloader, model, loss_fn, optimizer, args, t)
         test(test_dataloader, model, loss_fn)
     print("Done!")
     
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer, args, epoch):
     size = len(dataloader.dataset)
     model.train()
+    if args.lr_scheduler == 'step':
+        adjust_learning_rate(optimizer, epoch+1 , args )
+    elif args.lr_scheduler is None: 
+        pass  
+    else:
+        raise NotImplementedError()
+    
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
 
@@ -75,7 +83,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss:>7f} lr: {optimizer.param_groups[0]['lr']} [{current:>5d}/{size:>5d}]")
             
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -92,6 +100,20 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     
+def get_args_parser():
+    parser = argparse.ArgumentParser('argument for torch demo ')
+    parser.add_argument('--optimizer', default='sgd', type=str)
+    parser.add_argument('--lr_scheduler', default='step', type=str)
+    parser.add_argument('--epochs', default=6, type=int)
+    parser.add_argument('--base_lr', default=1e-2, type=float)
+    parser.add_argument('--lrschduler', default=False, type=bool)
+    parser.add_argument('--batch_size', default=64, type=int)
+    
+    args = parser.parse_args()
+    
+    return args
+    
 if __name__ == "__main__":
-    main()
+    args = get_args_parser()
+    main(args)
 
